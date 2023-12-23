9d9404765fd151ded5ef788a50c1d2ff27bed69b
arnold-anand <pagadalaarnold@karunya.edu.in>
2023-12-23 21:27:51 +0530

    skills and experience

--- src/App.js
+++ src/App.js
@@ -9,13 +9,17 @@ function App() {
       <div class="relative w-full h-28">
         <div class="absolute inset-0 bg-gradient-to-r from-[#f9e8ea] from-3% via-[#f09819] via-20% to-[#f793e2] to-90%">
           <div className="flex justify-center left-1/2 mt-5">
-            <img className="w-20 md:w-32 lg:w-48" src="signature.png" alt="" />
+            <img
+              className="w-20 md:w-32 lg:w-48"
+              src="./images/signature.png"
+              alt=""
+            />
           </div>
         </div>
         <div class="absolute inset-0 bg-gradient-to-b from-transparent to-white"></div>
       </div>
       {/* Landing */}
-      <div className="p-5 md:p-10 lg:lg:px-[17%] lg:py-0">
+      <div className="p-5 md:p-10 lg:px-36 lg:py-0">
         <div className="flex justify-between items-center">
           <div className="font-poppins font-extrabold text-3xl w-1/2">
             <div className="text-5xl arnold md:text-7xl lg:text-8xl">
@@ -26,7 +30,7 @@ function App() {
           <div>
             <img
               className="freelancer md:w-56 lg:w-96"
-              src="freelancer.svg"
+              src="./images/freelancer.svg"
               alt=""
             />
           </div>
@@ -46,13 +50,17 @@ function App() {
       <div className="mt-10 lg:mt-28">
         <div>
           <div className="font-poppins text-center w-full text-5xl font-extrabold flex justify-center">
-            <span>Projects</span>{" "}
-            <img className="w-20 -mt-10" src="projects.svg" alt="" />
+            <span>Projects</span>
+            <img className="w-20 -mt-10" src="./images/projects.svg" alt="" />
           </div>
         </div>
         <div className="font-poppins grid md:grid-cols-2 lg:grid-cols-3 lg:px-36 ">
           <div className="flex flex-col p-5 my-5 max-w-sm">
-            <img className="w-full rounded" src="chat-with-pdf.png" alt="" />
+            <img
+              className="w-full rounded"
+              src="./images/chat-with-pdf.png"
+              alt=""
+            />
             <div className="text-xl font-roboto p-2">
               {" "}
               Chat With PDF{" "}
@@ -71,7 +79,7 @@ function App() {
                 href="https://github.com/arnold-anand/Chat-with-pdf/blob/master/chat-with-pdf.ipynb"
               >
                 <button className="flex items-center justify-center p-2 border rounded space-x-2">
-                  <img className="w-8" src="github-mark.svg" alt="" />
+                  <img className="w-8" src="./images/github-mark.svg" alt="" />
                   <div>Github</div>
                 </button>
               </a>
@@ -80,7 +88,7 @@ function App() {
                 href="https://huggingface.co/spaces/arnold-anand/chat-with-pdf"
               >
                 <button className="flex items-center justify-center p-2 border rounded space-x-2">
-                  <img className="w-8" src="live.svg" alt="" />
+                  <img className="w-8" src="./images/live.svg" alt="" />
                   <div>Live Demo</div>
                 </button>
               </a>
@@ -89,7 +97,7 @@ function App() {
           <div className="flex flex-col p-5 my-5 max-w-sm">
             <img
               className="w-full h-48 rounded"
-              src="digit-recognition.png"
+              src="./images/digit-recognition.png"
               alt=""
             />
             <div className="text-xl font-roboto p-2">
@@ -108,7 +116,7 @@ function App() {
                 href="https://github.com/arnold-anand/Chat-with-pdf/blob/master/chat-with-pdf.ipynb"
               >
                 <button className="flex items-center justify-center p-2 border rounded space-x-2">
-                  <img className="w-8" src="github-mark.svg" alt="" />
+                  <img className="w-8" src="./images/github-mark.svg" alt="" />
                   <div>Github</div>
                 </button>
               </a>
@@ -117,14 +125,14 @@ function App() {
                 href="https://huggingface.co/spaces/arnold-anand/chat-with-pdf"
               >
                 <button className="flex items-center justify-center p-2 border rounded space-x-2">
-                  <img className="w-8" src="live.svg" alt="" />
+                  <img className="w-8" src="./images/live.svg" alt="" />
                   <div>Live Demo</div>
                 </button>
               </a>
             </div>
           </div>
           <div className="flex flex-col p-5 my-5 max-w-sm">
-            <img className="w-full rounded" src="isu.png" alt="" />
+            <img className="w-full rounded" src="./images/isu.png" alt="" />
             <div className="text-xl font-roboto p-2">
               Smart Phone Price Prediction
               <span className="p-[3px] px-2 bg-slate-300 text-xs rounded mx-1">
@@ -141,7 +149,7 @@ function App() {
                 href="https://www.kaggle.com/code/arnoldanand/smart-phone-price-prediction#Hyper-parameter-Tuning"
               >
                 <button className="flex items-center justify-center p-2 border rounded space-x-2">
-                  <img className="w-5" src="kaggle-icon.svg" alt="" />
+                  <img className="w-5" src="./images/kaggle-icon.svg" alt="" />
                   <div>Kaggle</div>
                 </button>
               </a>
@@ -170,30 +178,104 @@ function App() {
                 href="https://github.com/arnold-anand/Brain-Tumour-Detection"
               >
                 <button className="flex items-center justify-center p-2 border rounded space-x-2">
-                  <img className="w-8" src="github-mark.svg" alt="" />
+                  <img className="w-8" src="./images/github-mark.svg" alt="" />
                   <div>Github</div>
                 </button>
               </a>
             </div>
           </div>
 
-
           <div className="flex flex-col p-5 my-5 max-w-sm">
-            <img className="w-full rounded" src="icraae.png" alt="" />
-            <div className="text-xl font-roboto p-2">ICRAAE'24 Website <span className="p-[3px] px-2 bg-slate-300 text-xs rounded">React JS</span></div>
-            <div className="px-2 text-justify">Developed a multi-page web application for ICRAAE-2024 organized by the
-Dept. Aerospace Engineering at KITS</div>
+            <img className="w-full rounded" src="./images/icraae.png" alt="" />
+            <div className="text-xl font-roboto p-2">
+              ICRAAE'24 Website{" "}
+              <span className="p-[3px] px-2 bg-slate-300 text-xs rounded">
+                React JS
+              </span>
+            </div>
+            <div className="px-2 text-justify">
+              Developed a multi-page web application for ICRAAE-2024 organized
+              by the Dept. Aerospace Engineering at KITS
+            </div>
             <div className="flex items-center justify-between py-3">
               <a target="_blank" href="https://karunya.edu/daero/icraae-24/">
-              <button className="flex items-center justify-center p-2 border rounded space-x-2">
-                <img className="w-8" src="live.svg" alt="" />
-                <div>Live Website</div>
-              </button>
+                <button className="flex items-center justify-center p-2 border rounded space-x-2">
+                  <img className="w-8" src="./images/live.svg" alt="" />
+                  <div>Live Website</div>
+                </button>
               </a>
             </div>
           </div>
         </div>
       </div>
+      {/* Skills */}
+      <div className="w-full flex flex-col-reverse items-center justify-center">
+        <div className="font-poppins text-center text-5xl font-extrabold flex justify-center mt-10">
+          Skills
+        </div>
+        <img
+          className="md:w-24 ml-60 md:ml-[80%]"
+          src="./images/moon.svg"
+          alt=""
+        />
+      </div>
+      <div className=" flex items-center space-x-8 md:space-x-16 lg:space-x-32 flex-wrap justify-center p-5 md:px-20 lg:px-48">
+        <img className="w-12 m-5" src="./images/html.svg" alt="" />
+        <img className="w-12 m-5" src="./images/css.svg" alt="" />
+        <img className="w-12 m-5" src="./images/js.svg" alt="" />
+        <img className="m-5" src="./images/react.svg" alt="" />
+        <img className="m-5" src="./images/tailwind.svg" alt="" />
+        <img className="m-5" src="./images/supabase-icon.svg" alt="" />
+        <img className="w-20 m-5" src="./images/mysql-official.svg" alt="" />
+        <img className="w-12 m-5" src="./images/python-vertical.svg" alt="" />
+        <img className="m-5" src="./images/flask.svg" alt="" />
+        <img className="m-5" src="./images/git-scm-icon.svg" alt="" />
+      </div>
+
+      {/* Experience */}
+      <div className="w-full flex flex-col-reverse items-center justify-center">
+        <div className="font-poppins text-center text-5xl font-extrabold flex justify-center mt-10">
+          Experience & Education
+        </div>
+        <img
+          className="w-28 lg:w-36 mr-48 md:mr-[80%]"
+          src="./images/sun.svg"
+          alt=""
+        />
+      </div>
+
+      <div className="md:flex items-center lg:px-36">
+        <div>
+          <img className="max-w-sm" src="./images/studying.svg" alt="" />
+        </div>
+      <div>
+        <div className="flex space-x-8 items-center p-5">
+          <img className="w-10" src="./images/hitachi-icon.svg" alt="" />
+          <div>
+            <div className="text-xs">June 2023 - July 2023</div>
+            <div>AI Intern</div>
+            <div className="text-sm">Hitachi Vantara, Hyderabad</div>
+          </div>
+        </div>
+        <div className="flex space-x-8 items-center p-5">
+          <img className="w-10" src="./images/intel-icon.svg" alt="" />
+          <div>
+            <div className="text-xs">April 2023 - June 2023</div>
+            <div>AI Intern</div>
+            <div className="text-sm">Intel Unnati Training Program Summer</div>
+          </div>
+        </div>
+        <div className="flex space-x-8 items-center p-5">
+          <img className="w-10" src="https://viestories.com/wp-content/uploads/2022/05/f-72.jpg" alt="" />
+          <div>
+            <div className="text-xs">June 2022 - July 2022</div>
+            <div>Teaching Instructor</div>
+            <div className="text-sm">Camp K12</div>
+          </div>
+        </div>
+      </div>
+      </div>
+
     </div>
   );
 }
